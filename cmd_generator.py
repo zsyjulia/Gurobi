fdb8bf1642e293f29b83ebb69defb11f8105b00d
Shiyu Zhang <zsyjulia@Shiyus-MacBook-Pro.local>
2017-06-12 02:25:45 -0400

    ll

--- cmd_generator.py
+++ cmd_generator.py
@@ -9,12 +9,12 @@ list_gml = ['swan']
 
 f = file ('make_output_dir.sh' , 'w')
 
-for g in list_scheduler:
+for g in list_gml:
     
-    for j in list_gml:
+    for j in list_jobs:
         #print '\n--scheduler: ' + s + ' -gml: ' + g
         
-        for s in list_jobs:             
+        for s in list_scheduler:             
             f.write('mkdir -p ~/output/' + g+'/'+j+'/'+s+'\n'  )
             print 'java -cp target/gaia_ctrl-jar-with-dependencies.jar gaiasim.GaiaSim -g data/gml/' + g + '.gml -j data/combined_traces_fb/' + j + '-' + g +'.txt -s ' + s + ' -o ' + '~/output/' + g+'/'+j+'/'+s + ' > ~/output/' + g+'/'+j+'/'+s+'/exec_log.txt'
 
